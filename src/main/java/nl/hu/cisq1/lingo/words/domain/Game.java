b615226f890abf390a508418518c4e68dae24cc8
RainbowJM <Jevona98@live.com>
2021-02-12 21:32:24 +0100

    added more to the domain

--- src/main/java/nl/hu/cisq1/lingo/words/domain/Game.java
+++ src/main/java/nl/hu/cisq1/lingo/words/domain/Game.java
@@ -1,4 +1,21 @@
 package nl.hu.cisq1.lingo.words.domain;
 
 public class Game {
+    private Attempt attempt;
+
+//    public Long calculateScore(){
+//        switch (attempt){
+//
+//            case ONE:
+//                break;
+//            case TWO:
+//                break;
+//            case THREE:
+//                break;
+//            case FOUR:
+//                break;
+//            case FIVE:
+//                break;
+//        }
+//    }
 }
