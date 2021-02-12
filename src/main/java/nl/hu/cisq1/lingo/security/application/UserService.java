3530b5799612c116b763d89ca566b7d602738adc
RainbowJM <Jevona98@live.com>
2021-02-12 21:22:17 +0100

    added domain

---
+++ src/main/java/nl/hu/cisq1/lingo/security/application/UserService.java
@@ -0,0 +1,9 @@
+package nl.hu.cisq1.lingo.security.application;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserService {
+}
