865637698dd9a5782f4d03b6f595a238c0c0faa4
RainbowJM <Jevona98@live.com>
2021-03-10 00:06:34 +0100

    added data

--- src/main/java/nl/hu/cisq1/lingo/security/data/SpringUserRespository.java
+++ src/main/java/nl/hu/cisq1/lingo/security/data/SpringUserRespository.java
@@ -2,5 +2,8 @@ package nl.hu.cisq1.lingo.security.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SpringUserRespository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
