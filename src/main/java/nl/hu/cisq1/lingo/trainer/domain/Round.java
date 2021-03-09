8cad2f795d017ca1de82f0e0c03bfec69ddbf490
RainbowJM <Jevona98@live.com>
2021-03-10 00:07:17 +0100

    working on the Round domain class and test

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Round.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Round.java
@@ -2,6 +2,7 @@ package nl.hu.cisq1.lingo.trainer.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Round {
     private String wordToGuess;
@@ -14,10 +15,31 @@ public class Round {
         this.feedbacks = feedbacks;
     }
 
-    public void guess(String attempt){
-        if (getAttempt() <= 5){
+    public void guess(String attemptWord) {
+        int maxAttempt = 5;
 
+        if (getAttempt() <= maxAttempt) {
+            String[] lettersWordToGuess = this.wordToGuess.split("");
+            String[] lettersAttemptWord = attemptWord.split("");
+            List<Mark> mark = new ArrayList<>();
+
+            for (int i = 0; i < lettersWordToGuess.length; i++) {
+                String letterWordToGuess = lettersWordToGuess[i];
+                String letterAttemptWord = lettersAttemptWord[i];
+
+                if (letterAttemptWord.equals(letterWordToGuess)) {
+                    Mark markOfLetter = Mark.CORRECT;
+                    mark.add(markOfLetter);
+                }
+            }
+            Feedback feedbackList = new Feedback(mark, attemptWord);
+            feedbacks.add(feedbackList);
         }
+    }
+
+    public String giveHint() {
+
+        return null;
     }
 
     public int getAttempt() {
@@ -26,5 +48,31 @@ public class Round {
 
     public List<Feedback> getFeedbackHistory() {
         return feedbacks;
+    }
+
+    public int currentWordLength() {
+        return wordToGuess.length();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Round round = (Round) o;
+        return attempt == round.attempt && Objects.equals(wordToGuess, round.wordToGuess) && Objects.equals(feedbacks, round.feedbacks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(wordToGuess, attempt, feedbacks);
+    }
+
+    @Override
+    public String toString() {
+        return "Round{" +
+                "wordToGuess='" + wordToGuess + '\'' +
+                ", attempt=" + attempt +
+                ", feedbacks=" + feedbacks +
+                '}';
     }
 }
