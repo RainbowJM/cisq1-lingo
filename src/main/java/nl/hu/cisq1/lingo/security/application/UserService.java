cf4f717c4319885ae91323f90d20db4aff1dc78b
RainbowJM <Jevona98@live.com>
2021-03-10 15:11:19 +0100

    Revert "changed the coverage max and min"
    This reverts commit 8d44dc5c

--- src/main/java/nl/hu/cisq1/lingo/security/application/UserService.java
+++
@@ -1,9 +0,0 @@
-package nl.hu.cisq1.lingo.security.application;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@Transactional
-public class UserService {
-}
