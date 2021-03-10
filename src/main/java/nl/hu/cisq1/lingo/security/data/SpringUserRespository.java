7ca9b6464e8d4fe67d9729344747ce0d03757d80
RainbowJM <Jevona98@live.com>
2021-03-10 15:37:30 +0100

    Revert "changed the coverage max and min"
    This reverts commit 8d44dc5c

--- src/main/java/nl/hu/cisq1/lingo/security/data/SpringUserRespository.java
+++
@@ -1,9 +0,0 @@
-package nl.hu.cisq1.lingo.security.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface SpringUserRespository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-}
--- src/main/java/nl/hu/cisq1/lingo/security/presentation/dto/Login.java
+++
@@ -1,6 +0,0 @@
-package nl.hu.cisq1.lingo.security.presentation.dto;
-
-public class Login {
-    public String username;
-    public String password;
-}
