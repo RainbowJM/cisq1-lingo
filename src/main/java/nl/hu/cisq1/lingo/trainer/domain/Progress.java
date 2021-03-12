9435820bc06a13ccf7f3f1092bed0fa182954480
RainbowJM <Jevona98@live.com>
2021-03-12 09:45:19 +0100

    added Progress to the domain

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Progress.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Progress.java
@@ -1,7 +1,35 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
 public class Progress {
-    private long score;
+    private Long id;
+    private Long score;
     private String hints;
     private int roundNumber;
+
+    public Progress(Long id, Long score, String hints, int roundNumber) {
+        this.id = id;
+        this.score = score;
+        this.hints = hints;
+        this.roundNumber = roundNumber;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getScore() {
+        return score;
+    }
+
+    public String getHints() {
+        return hints;
+    }
+
+    public int getRoundNumber() {
+        return roundNumber;
+    }
 }
