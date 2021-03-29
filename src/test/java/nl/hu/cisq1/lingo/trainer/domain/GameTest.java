d967084f8d7161e9a310803a26cd0b038f866ce2
RainbowJM <Jevona98@live.com>
2021-03-29 02:01:02 +0200

    coverage higher

--- src/test/java/nl/hu/cisq1/lingo/trainer/domain/GameTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/GameTest.java
@@ -2,10 +2,40 @@ package nl.hu.cisq1.lingo.trainer.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
+import java.util.stream.Stream;
+
+import static nl.hu.cisq1.lingo.trainer.domain.Mark.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
+
+    @ParameterizedTest
+    @MethodSource("provideGuessingExamples")
+    @DisplayName("guessing a word")
+    void guessing(String wordToGuess, String attempt, List<Mark> expectedMarks) {
+        Game game = new Game();
+        game.startNewRound(wordToGuess);
+        game.guess(attempt);
+
+        Feedback expected = new Feedback(expectedMarks, attempt);
+
+        assertEquals(expected, game.showProgress().getFeedback().get(game.showProgress().getFeedback().size() - 1));
+    }
+
+    static Stream<Arguments> provideGuessingExamples() {
+        return Stream.of(
+                Arguments.of("BAARD", "BAARD", List.of(CORRECT, CORRECT, CORRECT, CORRECT, CORRECT)),
+                Arguments.of("BAARD", "BARST", List.of(CORRECT, CORRECT, PRESENT, ABSENT, ABSENT)),
+                Arguments.of("BAARD", "DRAAD", List.of(ABSENT, PRESENT, CORRECT, PRESENT, CORRECT)),
+                Arguments.of("BAARD", "BONJE", List.of(CORRECT, ABSENT, ABSENT, ABSENT, ABSENT)),
+                Arguments.of("BAARD", "BARAA", List.of(CORRECT, CORRECT, PRESENT, PRESENT, ABSENT))
+        );
+    }
 
     @Test
     @DisplayName("game status is eliminated")
@@ -20,6 +50,8 @@ class GameTest {
         game.guess("BARAA");
 
         assertTrue(game.isPlayerEliminated());
+        assertEquals(GameStatus.ELIMINATED, game.getStatus());
+
     }
 
     @Test
