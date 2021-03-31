6f509b93cf3e9bb349babb2edc328edabe51f9d0
RainbowJM <Jevona98@live.com>
2021-04-01 00:13:28 +0200

    working on controller test

--- src/test/java/nl/hu/cisq1/lingo/trainer/presentation/TrainerControllerWebTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/presentation/TrainerControllerWebTest.java
@@ -1,43 +1,61 @@
-//package nl.hu.cisq1.lingo.trainer.presentation;
-//
-//import nl.hu.cisq1.lingo.CiTestConfiguration;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.context.annotation.Import;
-//
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.RequestBuilder;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//
-//import static org.hamcrest.Matchers.*;
-//import static org.mockito.Mockito.when;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@SpringBootTest
-//@Import(CiTestConfiguration.class)
-//@AutoConfigureMockMvc
-//class TrainerControllerWebTest {
-//    @Autowired
-//    private MockMvc mockMvc;
-//
+package nl.hu.cisq1.lingo.trainer.presentation;
+
+import nl.hu.cisq1.lingo.CiTestConfiguration;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@Import(CiTestConfiguration.class)
+@AutoConfigureMockMvc
+class TrainerControllerWebTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("start a new game")
+    void startNewGame() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/trainer/games");
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", greaterThanOrEqualTo(0)))
+                .andExpect(jsonPath("$.score", is(0)))
+                .andExpect(jsonPath("$.status", is("PLAYING")))
+                .andExpect(jsonPath("$.feedback", hasSize(0)))
+                .andExpect(jsonPath("$.hint", hasLength(5)));
+    }
+
 //    @Test
-//    @DisplayName("start a new game")
-//    void startNewGame() throws Exception{
+//    @DisplayName("guessing a word after starting a game")
+//    void guess() throws Exception{
 //        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/trainer/games");
 //
-//        mockMvc.perform(requestBuilder)
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.gameId", greaterThanOrEqualTo(0)))
-//                .andExpect(jsonPath("$.score", is(0)))
-//                .andExpect(jsonPath("$.status", is("PLAYING")))
-//                .andExpect(jsonPath("$.feedbackHistory", hasSize(0)))
-//                .andExpect(jsonPath("$.currentHint",hasSize(5)))
-//                .andExpect(jsonPath("$.attemptsLeft",is(5)));
-//    }
+//        MockHttpServletResponse response = mockMvc.perform(requestBuilder).andReturn().getResponse();
+//        int id = JsonPath.read(response.getContentAsString(), "$.id" );
 //
-//}
+//        String guessJson = "{\"attempt\":\"barst\"}";
+//
+//        String guessBody = new ObjectMapper().writeValueAsString(guessJson);
+//
+//        RequestBuilder guessRequest = MockMvcRequestBuilders
+//                .post("/trainer/games/" + id + "/round/guess")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(guessBody);
+//
+//        mockMvc.perform(guessRequest)
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$.id",greaterThanOrEqualTo(0)))
+//                .andExpect(jsonPath("$.feedback", hasSize(1)));
+//    }
+}
