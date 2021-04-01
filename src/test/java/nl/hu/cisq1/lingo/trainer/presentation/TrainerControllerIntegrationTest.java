067b54ceb08545604d7af7530ecff5cb86c6a37c
RainbowJM <Jevona98@live.com>
2021-04-02 00:12:19 +0200

    working

--- src/test/java/nl/hu/cisq1/lingo/trainer/presentation/TrainerControllerIntegrationTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/presentation/TrainerControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package nl.hu.cisq1.lingo.trainer.presentation;
 
 import nl.hu.cisq1.lingo.trainer.data.SpringGameRepository;
+import nl.hu.cisq1.lingo.trainer.domain.Game;
 import nl.hu.cisq1.lingo.words.data.SpringWordRepository;
 import nl.hu.cisq1.lingo.words.domain.Word;
 import org.junit.jupiter.api.DisplayName;
@@ -52,31 +53,31 @@ public class TrainerControllerIntegrationTest {
                 .andExpect(jsonPath("$.feedback", hasSize(0)));
     }
 
-//    @Test
-//    @DisplayName("start new round")
-//    void startNewRound() throws Exception {
-//        Game game = new Game();
-//        game.startNewRound("BAARD");
-//        game.guess("BAARD");
-//
-//        when(gameRepository.findById(0L))
-//                .thenReturn(Optional.of(game));
-//
-//        when(wordRepository.findRandomWordByLength(6))
-//                .thenReturn(Optional.of(new Word("HOEDEN")));
-//
-//        RequestBuilder request = MockMvcRequestBuilders.post("/trainer/games/0/round");
-//
-//        String hint = "H.....";
-//
-//        mockMvc.perform(request).
-//                andExpect(status().isOk()).
-//                andExpect(jsonPath("$.status", is("PLAYING")))
+    @Test
+    @DisplayName("start new round")
+    void startNewRound() throws Exception {
+        Game game = new Game();
+        game.startNewRound("BAARD");
+        game.guess("BAARD");
+
+        when(gameRepository.findById(0L))
+                .thenReturn(Optional.of(game));
+
+        when(wordRepository.findRandomWordByLength(6))
+                .thenReturn(Optional.of(new Word("HOEDEN")));
+
+        RequestBuilder request = MockMvcRequestBuilders.post("/trainer/games/0/round");
+
+        String hint = "H.....";
+
+        mockMvc.perform(request).
+                andExpect(status().isOk()).
+                andExpect(jsonPath("$.status", is("PLAYING")))
 //                .andExpect(jsonPath("$.score", is(25)))
-//                .andExpect(jsonPath("$.hint", hasSize(6)))
-//                .andExpect(jsonPath("$.hint", equalTo(hint)))
-//                .andExpect(jsonPath("$.feedback", hasSize(0)));
-//    }
+                .andExpect(jsonPath("$.hint", hasLength(6)))
+                .andExpect(jsonPath("$.hint", equalTo(hint)))
+                .andExpect(jsonPath("$.feedback", hasSize(0)));
+    }
 
     @Test
     @DisplayName("cannot start new round if game not found")
