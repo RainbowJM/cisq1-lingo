a46366db2483136e1caabf6eb451b099a3a45825
RainbowJM <Jevona98@live.com>
2021-04-10 13:18:23 +0200

    removed not used methods

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/exception/InvalidAction.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/exception/InvalidAction.java
@@ -1,9 +1,6 @@
 package nl.hu.cisq1.lingo.trainer.domain.exception;
 
 public class InvalidAction extends RuntimeException {
-    public InvalidAction() {
-    }
-
     public InvalidAction(String message) {
         super(message);
     }
--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/exception/InvalidWordLength.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/exception/InvalidWordLength.java
@@ -3,8 +3,4 @@ package nl.hu.cisq1.lingo.trainer.domain.exception;
 public class InvalidWordLength extends RuntimeException {
     public InvalidWordLength() {
     }
-
-    public InvalidWordLength(String message) {
-        super(message);
-    }
 }
